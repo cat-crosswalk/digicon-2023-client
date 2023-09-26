e05f34c7b1ef9373d25813570fef10c41f0b07ea
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2023-09-26 15:14:18 +0900

    ✨ アイコン入れる

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -1,30 +1,43 @@
 'use client'
 
+import AddCircleIcon from '@material-symbols/svg-400/outlined/add_circle.svg'
+import FavoriteIcon from '@material-symbols/svg-400/outlined/favorite.svg'
+import HomeIcon from '@material-symbols/svg-400/outlined/home.svg'
+import SearchIcon from '@material-symbols/svg-400/outlined/search.svg'
 import { clsx } from 'clsx'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import React from 'react'
 
 interface Route {
   path: string
-  icon: string
+  icon: React.ReactNode
 }
 
+const iconProps = Object.freeze({
+  className: clsx('w-6', 'h-6', 'fill-text-primary'),
+})
 const Routes = [
   {
     path: '/',
-    icon: 'home',
+    icon: <HomeIcon {...iconProps} />,
   },
   {
     path: '/search',
-    icon: 'search',
+    icon: <SearchIcon {...iconProps} />,
   },
   {
     path: '/create',
-    icon: 'create',
+    icon: <AddCircleIcon {...iconProps} />,
   },
   {
     path: '/favorite',
-    icon: 'favorite',
+    icon: <FavoriteIcon {...iconProps} />,
+  },
+  // FIXME: favorite にしてるのはてきとう trailing slash をつけて key の重複を防ぐ
+  {
+    path: '/favorite/',
+    icon: <FavoriteIcon {...iconProps} />,
   },
 ] as const satisfies readonly Route[]
 
@@ -32,27 +45,31 @@ export const Nav: React.FC = () => {
   const pathname = usePathname()
 
   return (
-    <nav className={clsx('bg-slate-100', 'py-4')}>
-      <ul className={clsx('grid', 'grid-cols-4', 'place-items-center')}>
+    <nav
+      className={clsx(
+        'bg-bg-primary',
+        'border-t-1',
+        'border-t-ui-primary',
+        'h-14',
+        'px-11'
+      )}
+    >
+      <ul className={clsx('flex', 'justify-between', 'h-full', 'items-center')}>
         {Routes.map(({ path, icon }) => (
-          <li
-            key={path}
-            className={clsx('w-10', 'h-10', 'bg-white', 'rounded-full')}
-          >
+          <li key={path} className={clsx()}>
             <Link
               aria-current={path === pathname ? 'page' : undefined}
               href={path}
               className={clsx(
-                'skeleton-lime-700',
                 'grid',
                 'place-items-center',
-                'w-full',
-                'h-full',
+                'w-10',
+                'h-10',
                 'aspect-square',
-                'rounded-full'
+                'text-text-primary'
               )}
             >
-              {/* TODO: Replace with icon */}
+              {icon}
             </Link>
           </li>
         ))}
