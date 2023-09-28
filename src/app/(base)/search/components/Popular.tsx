0f251b98b82552f6ce4bf02382ca625159cf7a3f
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2023-09-28 15:50:15 +0900

    🎨 タグを存在するものに

--- src/app/(base)/search/components/Popular.tsx
+++ src/app/(base)/search/components/Popular.tsx
@@ -1,19 +1,24 @@
 import { clsx } from 'clsx'
 
+import { getWorks } from '@/api/getWorks'
+
 import { PopularTagElement } from './PopularTagElement'
 
-const dummyPopularTags = [
-  'sunlight',
-  'forest',
-  'nature',
-  'landscape',
-  'tree',
-  'mountains',
-  'dark',
-  'light',
-]
 
-export const Popular: React.FC = () => {
+export const Popular: React.FC = async () => {
+  const works = await getWorks()
+
+  const tags = works.flatMap(({ tags }) => tags)
+  const tagCountMap = new Map<string, number>()
+  tags.forEach(tag => {
+    const count = tagCountMap.get(tag) ?? 0
+    tagCountMap.set(tag, count + 1)
+  })
+
+  const sortedTags = [...tagCountMap.entries()]
+    .sort((a, b) => b[1] - a[1])
+    .map(([tag]) => tag)
+
   return (
     <section
       className={clsx(
@@ -30,7 +35,7 @@ export const Popular: React.FC = () => {
         おすすめのタグ
       </h2>
       <div className={clsx('mt-4', 'grid', 'grid-cols-2', 'gap-2', 'w-full')}>
-        {dummyPopularTags.map(tag => (
+        {sortedTags.map(tag => (
           <PopularTagElement key={tag} tag={tag} />
         ))}
       </div>
