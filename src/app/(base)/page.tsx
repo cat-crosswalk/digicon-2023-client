063983bd2dc668478e10a97af709fccd64892c9c
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2023-09-28 15:05:33 +0900

    🎉 アイコン設定する

--- src/app/(base)/page.tsx
+++ src/app/(base)/page.tsx
@@ -1,6 +1,7 @@
 import { clsx } from 'clsx'
 
 import { getWorks } from '@/api/getWorks'
+import Icon from '@/assets/icon.svg'
 import { Preview } from '@/components/Preview'
 
 import type { NextPage } from 'next'
@@ -10,15 +11,7 @@ const Home: NextPage = async () => {
 
   return (
     <div className={clsx('flex', 'flex-col', 'items-center', 'py-8', 'gap-8')}>
-      <div
-        className={clsx(
-          'rounded-full',
-          'w-10',
-          'h-10',
-          'bg-slate-300',
-          'skeleton-orange-300'
-        )}
-      />
+      <Icon width={60} height={60} viewBox="0 0 512 512" />
       <div
         className={clsx(
           'grid',
